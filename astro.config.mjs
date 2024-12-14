d0993bcd941935df8e6001f5b2c5d667e001a297
Aayush Aryal <155847959+eyus00@users.noreply.github.com>
2024-12-14 08:52:50 +0200

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,38 @@
-import { defineConfig } from 'astro/config'
-import tailwind from '@astrojs/tailwind'
-import react from '@astrojs/react'
-import vercel from '@astrojs/vercel/serverless'
-import db from '@astrojs/db'
-import legacy from '@vitejs/plugin-legacy'
+import { defineConfig } from 'astro/config';
+import tailwind from '@astrojs/tailwind';
+import react from '@astrojs/react';
+import vercel from '@astrojs/vercel/serverless';
+import db from '@astrojs/db';
+import legacy from '@vitejs/plugin-legacy';
+import AstroPWA from '@vite-pwa/astro';
 
 // https://astro.build/config
 export default defineConfig({
   output: 'server',
   adapter: vercel(),
-  integrations: [tailwind(), db(), react()],
+  integrations: [
+    tailwind(),
+    db(),
+    react(),
+    AstroPWA({
+      manifest: {
+        name: "Wovie",
+        short_name: "Wovie",
+        description: "Watch Movies and Tv shows!",
+        start_url: "/",
+        display: "standalone",
+        background_color: "#2f68c5",
+        theme_color: "#2f68c5",
+        icons: [
+          {
+            src: "/icon.png",
+            sizes: "192x192",
+            type: "image/png"
+          }
+        ]
+      }
+    })
+  ],
   vite: {
     plugins: [
       legacy({
@@ -18,4 +41,4 @@ export default defineConfig({
       }),
     ],
   },
-})
+});
